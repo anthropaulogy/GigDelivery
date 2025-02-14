a43c805bb4957b677e2e8abb93b4321251482c12
Paul Schockner <paulschockner@gmail.com>
2025-02-14 05:36:56 -0800

    Added is_valid_location_name to verify location names

--- src/helpers/validation.py
+++ src/helpers/validation.py
@@ -1,1 +1,8 @@
 """A module containing functions that serve to validate some information."""
+
+import re
+
+
+def is_valid_location_name(name):
+    pattern = r"^[A-Za-z0-9&'., -]+$"
+    return bool(re.match(pattern, name))
