8aa606aca751a220531f2d1b4ea7170da9c0c679
Paul Schockner <paulschockner@gmail.com>
2025-02-14 04:09:02 -0800

    Added InvalidIDError to raise an exception when an invalid ID is used or entered

--- src/helpers/exceptions.py
+++ src/helpers/exceptions.py
@@ -1,1 +1,9 @@
 """A module containing custom exceptions"""
+
+
+class InvalidIDError(Exception):
+    """A exception for an invalid ID"""
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
