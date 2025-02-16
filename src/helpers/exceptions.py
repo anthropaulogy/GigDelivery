9cda1b866ec89f0cfda2ebb799ed3de5f40ed59d
Paul Schockner <paulschockner@gmail.com>
2025-02-16 00:06:58 -0800

    Added location-related exceptions

--- src/helpers/exceptions.py
+++ src/helpers/exceptions.py
@@ -1,6 +1,16 @@
 """A module containing custom exceptions"""
 
 
+class IDExistsError(Exception):
+    def __init__(self, message: str = "ID is not unique") -> None:
+        super().__init__(message)
+
+
+class IDNotFoundError(Exception):
+    def __init__(self, message: str = "ID was not found") -> None:
+        super().__init__(message)
+
+
 class InvalidIDError(Exception):
     def __init__(self, message: str = "Invalid ID") -> None:
         super().__init__(message)
@@ -8,4 +18,9 @@ class InvalidIDError(Exception):
 
 class InvalidLocationNameError(Exception):
     def __init__(self, message: str = "Invalid location name") -> None:
+        super().__init__(message)
+
+
+class InvalidLocationTypeError(Exception):
+    def __init__(self, message: str = "Invalid location type") -> None:
         super().__init__(message)
