d525431608d2e3972e7912ebccf598b42b52b24b
Paul Schockner <paulschockner@gmail.com>
2025-02-16 00:08:55 -0800

    Created validations module and added a function for location name and location type

--- src/helpers/validation.py
+++ src/helpers/validation.py
@@ -2,7 +2,13 @@
 
 import re
 
+from helpers import reference
 
-def is_valid_location_name(name):
+
+def is_valid_location_name(name) -> bool:
     pattern = r"^[A-Za-z0-9&'., -]+$"
     return bool(re.match(pattern, name))
+
+
+def is_valid_location_type(location_type: str) -> bool:
+    return location_type in reference.location_types
