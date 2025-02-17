8c7a14573fab4264cd1f23751de06c370d5e5bec
Paul Schockner <paulschockner@gmail.com>
2025-02-16 18:37:29 -0800

    Created reference module to hold data that will only be referenced by the code

---
+++ src/helpers/reference.py
@@ -0,0 +1,10 @@
+"""A module that has data to be referenced"""
+
+location_types = [
+    "Customer",
+    "Package Dropoff",
+    "Package Pickup",
+    "Restaurant",
+    "Shopping",
+    "Shopping - Pickup",
+]
